877c3c6b0d3c7462224f362e705f9ce5c8abc72d
yilinmiao <yi.matheson@gmail.com>
2017-09-01 18:08:02 -0700

    added more func

---
+++ ft_lstmap.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymiao <ymiao@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/01 17:37:36 by ymiao             #+#    #+#             */
+/*   Updated: 2017/09/01 17:37:38 by ymiao            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list * (*f)(t_list *elem))
+{
+	t_list	*root;
+	t_list	*cur;
+
+	root = cur = f(lst);
+	if (root == NULL)
+		return (NULL);
+	
+	while (lst->next) {
+		lst = lst->next;
+		cur->next = f(lst);
+		if (cur->next == NULL)
+			return (NULL);
+		cur = cur->next;
+	}
+	return (root);
+}
---
+++ ft_lstnew.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymiao <ymiao@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/01 17:09:04 by ymiao             #+#    #+#             */
+/*   Updated: 2017/09/01 17:09:05 by ymiao            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*list;
+
+	list = (t_list*)malloc(sizeof(t_list));
+	if (list == NULL)
+		return (NULL);
+	
+	if (content == NULL) {
+		list->content = NULL;
+		list->content_size = 0;
+	} else {
+		list->content = malloc(content_size);
+		if (list->content == NULL)
+			return (NULL);
+		ft_memcpy(list->content, content, content_size);
+		list->content_size = content_size;
+	}
+	list->next = NULL;
+	return (list);
+}
